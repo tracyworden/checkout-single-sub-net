e2782e3eb009626797d6a4764986fe0fec0cb2b9
tracy.worden@machiningcloud.com <Tracy.Worden@machiningcloud.com>
2022-03-11 14:08:33 -0800

    commit on master

---
+++ server/Models/ConfigResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+public class ConfigResponse
+{
+    [JsonProperty("publishableKey")]
+    public string PublishableKey { get; set; }
+
+    [JsonProperty("proPrice")]
+    public string ProPrice { get; set; }
+
+    [JsonProperty("basicPrice")]
+    public string BasicPrice { get; set; }
+}
